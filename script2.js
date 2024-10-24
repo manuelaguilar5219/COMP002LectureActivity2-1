db20aae8402a87f0119b86fd815954900e1fcf03
Manuel Aguilar <manuelaguilar5219@my.whccd.edu>
2024-10-23 19:52:51 -0700

    script2.js for lecture activity 2-2

---
+++ script2.js
@@ -0,0 +1,14 @@
+// Lecture 2-2
+// String Variable
+let myGreeting = "Hello World!"
+console.log(myGreeting);
+
+// Change the string variable value
+myGreeting = "I changed the value of the string Variable here";
+console.log(myGreeting);
+
+// Const Variable
+const pi = 3.14;
+// pi = 2; // throws an error
+myNumber = "5"
+console.log(Math.min(pi, myNumber)); // It Chooses the smaller number
