a2a4aa9d8be5891c7238e6257ad985e3c62e7357
Manuel Aguilar <manuelaguilar5219@my.whccd.edu>
2024-10-23 20:46:23 -0700

    conditional-code.js for Lecture Activity 2-3

---
+++ js/conditional-code.js
@@ -0,0 +1,13 @@
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+for(let i = 10; i >= 0; i--){
+    console.log(i);
+}
+
+let outputVariable = "";
+for(let i = 0; i <= 10; i++){
+    outputVariable += "!"; // Same as outputVariable = outputVariable + "!";
+    console.log(outputVariable);
+}
