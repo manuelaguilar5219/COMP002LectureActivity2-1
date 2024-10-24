a42a4d9f314c4e6a10023d925a309158f86eb04e
Manuel Aguilar <manuelaguilar5219@my.whccd.edu>
2024-10-23 19:05:25 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// This is a comment-line
+/*
+This
+is
+a
+multi-line
+comment
+*/
+console.log("hello world!");
+console.log(8* null); // result: 0
+console.log("5" - 1); // result: 4
+console.log("5" + 1); //result: 51
+console.log("five" * 2); // result: NaN
+console.log(false == 0); // result: true
+console.log(5 > 2 && 6 > 3); // result: true
+console.log(false == false ? "equal" : "not equal") // result: equal
+console.log(true || false); // result: true
